879146d5bea738d6871a8cf2b2c07bfb8fa6cd5d
Ricardo Marquez <rikmarqk@gmail.com>
2020-09-16 05:43:21 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -20,17 +20,6 @@ end
 
 introduction_with_language_optional("Edwin")
 
-def introduction_with_language_optional(name, language = "Ruby")
-  puts "Hi, my name is #{name} and I am learning to program in #{language}"
-end
-
-introduction_with_language_optional("Edwin", "Python")
-
-
-
-
-
-
 
 
 
